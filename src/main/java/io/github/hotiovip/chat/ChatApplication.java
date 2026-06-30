9d43d3f94d2d0d1ed57530734ce8dcfec4c9bce0
Hotiovip <hotiovipita@gmail.com>
2026-06-30 16:42:44 +0200

    Main Commit

---
+++ src/main/java/io/github/hotiovip/chat/ChatApplication.java
@@ -0,0 +1,14 @@
+package io.github.hotiovip.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
